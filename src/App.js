c0057baac705d1bd1cbb90ba1d7eab38daf2f526
arfian-rp <arfianrafi77@gmail.com>
2021-08-14 02:18:06 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+function App(props) {
+  const [data, setData] = useState({});
+  const getData = async () => {
+    try {
+      let { data } = await axios.get("https://api.openweathermap.org/data/2.5/weather?q=surabaya&units=imperial&appid=4ad9126a1f97c33e5930db5297993398");
+      setData(data);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <center>
+        <h3>kota: {data.name}</h3>
+        <h4>temp: {data.main.temp}</h4>
+        <h4>temp min: {data.main.temp_min}</h4>
+        <h4>temp max: {data.main.temp_max}</h4>
+      </center>
     </div>
   );
 }
